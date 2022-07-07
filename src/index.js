95c093ed48f5f4b7a8bd3121a8e284cfa189ef12
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-07-06 21:53:56 -0300

    chore: init config index

---
+++ src/index.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const port = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
