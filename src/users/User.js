b3939a36fbc9fec04e64e5c4d14f802f2d7fbd75
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-07-06 22:15:21 -0300

    chore: add model User

---
+++ src/users/User.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, select: false },
+  avatar: { type: String },
+});
+
+const User = mongoose.model("User", UserSchema, "users");
+
+module.exports = User;
