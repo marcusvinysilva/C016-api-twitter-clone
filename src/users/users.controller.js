a3c6e120631bbbca65afc8a37fab2154122743bd
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-07-06 22:53:33 -0300

    feat: add create user controller ans service

--- src/users/users.controller.js
+++ src/users/users.controller.js
@@ -1,5 +1,35 @@
-const createUser = (req, res) => {
-  res.send({ message: "rota create ok" });
+const userService = require("./users.service");
+
+const createUser = async (req, res) => {
+  const { name, username, email, password, avatar } = req.body;
+
+  if (!name || !username || !email || !password) {
+    return res.status(400).send({
+      message:
+        "Alguns campos estão faltando. Os campos obrigatórios são: 'name', 'username', 'email' e 'password' ",
+    });
+  }
+
+  const foundUserByEmail = await userService.findByEmail(email);
+  const foundUserByUsername = await userService.findByUsername(username);
+
+  if (foundUserByEmail || foundUserByUsername) {
+    return res
+      .status(400)
+      .send({ message: "Usuário com esse email/username já existe!" });
+  }
+
+  const user = await userService
+    .createUser(req.body)
+    .catch((error) => console.log(error));
+
+  if (!user) {
+    return res
+      .status(500)
+      .send({ message: "Aconteceu algum erro ao criar o usuário. Tente novamente mais tarde!" });
+  }
+
+  res.status(201).send(user);
 };
 
 module.exports = {
---
+++ src/users/users.service.js
@@ -0,0 +1,14 @@
+const User = require("./User");
+
+const createUser = async (body) => await User.create(body);
+
+const findByEmail = async (email) => await User.findOne({ email: email });
+
+const findByUsername = async (username) =>
+  await User.findOne({ username: username });
+
+module.exports = {
+  createUser,
+  findByEmail,
+  findByUsername,
+};
