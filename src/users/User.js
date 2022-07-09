7e68f82e55f2bc7a4b20ee782a05c9185c07d405
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-07-08 22:10:38 -0300

    chore(aula15): add bcrypt

--- src/users/User.js
+++ src/users/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs")
 
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
@@ -7,6 +8,11 @@ const UserSchema = new mongoose.Schema({
   password: { type: String, required: true, select: false },
   avatar: { type: String },
 });
+
+UserSchema.pre("save", async function(next) {
+  this.password = await bcrypt.hash(this.password, 10)
+  next()
+})
 
 const User = mongoose.model("User", UserSchema, "users");
 
