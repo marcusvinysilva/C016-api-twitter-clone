d079e875d491a5a2ee09fcbf2fb9572f80e027f0
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-07-13 21:00:36 -0300

    chore(aula18): add search tweets

--- src/tweets/tweets.service.js
+++ src/tweets/tweets.service.js
@@ -3,9 +3,18 @@ const Tweet = require("./Tweet");
 const createTweet = async (message, userId) =>
   await Tweet.create({ message, user: userId });
 
-const findAllTweets = async () => await Tweet.find().sort({ _id: -1 }).populate("user");
+const findAllTweets = async () =>
+  await Tweet.find().sort({ _id: -1 }).populate("user");
+
+const searchTweet = async (message) =>
+  await Tweet.find({
+    message: { $regex: `${message || ""}`, $options: "i" },
+  })
+    .sort({ _id: -1 })
+    .populate("user");
 
 module.exports = {
   createTweet,
   findAllTweets,
+  searchTweet,
 };
